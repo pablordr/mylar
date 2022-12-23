434f5928d139fd36d0ce268948be86f7a5339545
pablordr <pablordr@>
2022-12-23 13:34:55 +0100

    entries can be edited, honestly this is mostly like facebook

--- back.py
+++ back.py
@@ -4,13 +4,17 @@ import sqlite3
 
 app = Flask(__name__)
 
-def update_db(db_data):
+def update_db(db_data,action): 
     con = sqlite3.connect('mylar.db')
     cur = con.cursor()
+    # TODO: Remove this to make less calls in the future. Add to init script.
     q = "CREATE TABLE IF NOT EXISTS entries (id INT, title TEXT)"
     cur.execute(q)
     con.commit()
-    q = "INSERT INTO entries (id, title) VALUES (%s,'%s')" % (db_data['entry_id'], db_data['entry_title'])
+    if action == 'add':
+        q = "INSERT INTO entries (id, title) VALUES (%s,'%s')" % (db_data['entry_id'], db_data['entry_title'])
+    elif action == 'edit':
+        q = "UPDATE entries SET id=%s, title='%s' WHERE id=%s" % (db_data['entry_id'], db_data['entry_title'], db_data['entry_id'])
     cur.execute(q)
     con.commit()
     con.close()
@@ -40,14 +44,16 @@ def add_title():
             "entry_id": entry_id,
             "entry_title": entry_title
         }
-        update_db(db_data)
+        update_db(db_data, action="add")
         return render_template('add.html', msg="Data added successfully")
 
 @app.route("/entry", methods=["GET"])
 def show_entry():
     entry_id = request.args.get('id')
     if entry_id is None: 
-        return "No ID specified"
+        msg = "Error: No ID specified"
+        return render_template('error.html', msg=msg)
+
     else:
         res = query_db(entry_id)
         # Eval res
@@ -57,6 +63,32 @@ def show_entry():
         else:
             # return res saving this one for future api json response
             return render_template('entry_view.html', entry_id = res[0][0], entry_title = res[0][1])
+
+@app.route("/edit", methods=["GET","POST"])
+def edit_entry():
+    if request.method == "GET":
+        entry_id = request.args.get('id')
+        if entry_id is None:
+            msg = "Error: No ID specified."
+            return render_template('error.html', msg = msg)
+        else:
+            res = query_db(entry_id)
+            if len(res) == 0:
+                msg = "Error, no entry with that ID"
+                return render_template('error.html', msg = msg)
+            else:
+                return render_template('edit.html', entry_id = res[0][0], entry_title = res[0][1])
+    elif request.method == "POST":
+        form_data = request.form
+        entry_id =  form_data['entry_id']
+        entry_title = form_data['entry_title']
+        db_data = {
+            "entry_id": entry_id,
+            "entry_title": entry_title
+        }
+        update_db(db_data, action="edit")
+        return redirect('/entry?id=%s' % (entry_id))
+        return render_template('entry_view.html', msg="Data updated successfully")
 
 
 if __name__ == "__main__":
