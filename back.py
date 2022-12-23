e515f7f50191d50b8cc6d9bde6b509d0cac1deda
pablordr <pablordr@>
2022-12-23 09:49:39 +0100

    testing file access

--- back.py
+++ back.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def show_root():
     return "this is /"
+
+@app.route("/filewrite")
+def file_write():
+    f = open('file.txt','w')
+    now = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+    f.write(now)
+    f.close()
+    return "File Updated"
+    
+@app.route("/fileread")
+def file_read():
+    f = open('file.txt','r')
+    s = f.readlines()[0]
+    f.close()
+    return s
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- test.py
+++ test.py
@@ -1,6 +1,27 @@
 import requests
 
+print("Testing /")
+print(20*'-')
+
 response = requests.get('http://localhost:5000')
 
 print(response.status_code)
 print(response.text)
+
+print("\nTesting /filewrite")
+print(20*'-')
+
+response = requests.get('http://localhost:5000/filewrite')
+
+print(response.status_code)
+print(response.text)
+
+print("\nTesting /fileread")
+print(20*'-')
+
+response = requests.get('http://localhost:5000/fileread')
+
+print(response.status_code)
+print(response.text)
+
+
