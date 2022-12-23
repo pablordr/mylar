ee1785e1f4312c72671967aaf44face16f9ac1c9
pablordr <pablordr@>
2022-12-23 12:31:37 +0100

    entry view via url parameter /entry?id=<ID>, error template

--- back.py
+++ back.py
@@ -15,6 +15,15 @@ def update_db(db_data):
     con.commit()
     con.close()
 
+def query_db(entry_id):
+    con = sqlite3.connect('mylar.db')
+    cur = con.cursor()
+    q = "SELECT * FROM entries WHERE id=%s" % (entry_id)
+    cur.execute(q)
+    res = cur.fetchall()
+    con.close()
+    return res
+
 @app.route("/")
 def show_root():
     return "this is /"
@@ -33,6 +42,22 @@ def add_title():
         }
         update_db(db_data)
         return render_template('add.html', msg="Data added successfully")
+
+@app.route("/entry", methods=["GET"])
+def show_entry():
+    entry_id = request.args.get('id')
+    if entry_id is None: 
+        return "No ID specified"
+    else:
+        res = query_db(entry_id)
+        # Eval res
+        if len(res) == 0:
+            msg = "Error, no entry with that ID"
+            return render_template('error.html', msg = msg)
+        else:
+            # return res saving this one for future api json response
+            return render_template('entry_view.html', entry_id = res[0][0], entry_title = res[0][1])
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
